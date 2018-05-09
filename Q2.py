44120ee1797bba1357ebb3695623b4c0d19a46fa
Lucas Pontes <lpa@cin.ufpe.br>
2018-05-09 10:45:32 -0300

    modified:   Q1.py 	modified:   Q2.py 	new file:   Q3.py 	new file:   figure_Q2.png 	new file:   figure_Q3.png

--- Q2.py
+++ Q2.py
@@ -4,39 +4,50 @@ import matplotlib.pyplot as plt
 import scipy.constants as scc
 from scipy import integrate
 
+#funcao que calcula potecial discreto de um elemento de area em relacao a um ponto
+# R = distancia entre o elemento de area e o ponto
+# P = vetor que indica o ponto a ser medido o pontencial
+# r = ponto do elemento de area
+
 def potencial_disc(r,P,q):
     R = np.sqrt((P[0]-r[0])**2 +(P[1]-r[1])**2 + (P[2]-r[2])**2)
     return (1/(4*np.pi*scc.epsilon_0))*(q/R)
+
+#calcula o pontencial da area referente a um ponto P, sendo dividido em elementos de area com lados N
+#ro = densidade de carga superficial
+# 2*a = lado do quadrado equivalente a area total
 
 def potencial_total(N,P,ro,a):
     act = [a,a,0]
     pot = 0
     q = ro*(N*N)
-    while act[1] > -a:
+    while act[1] >= -a:
         act[0] = a
         while act[0] >= -a:
            pot = pot + potencial_disc(act,P,q)
            act[0] -= N
         act[1] -= N
     return pot
-# plot(x, y, color='green', linestyle='dashed', marker='o',
-#                      markerfacecolor='blue', markersize=12).
+#inicializacao das variaveis de acordo com o problema
 teta = [-np.pi/2]
-N = 0.001
-P = [0.0,np.cos(teta[0]),np.sin(teta[0])]
+N = 0.0001
+P = [0.0,np.sin(teta[0]),np.cos(teta[0])]
 a = 0.1
 ro = 10**(-5)
 res = []
 while teta[len(teta)-1] <= np.pi/2:
+    #coloca no vetor o pontencial correspondente ao ponto P
     res.append(potencial_total(N,P,ro,a))
-    teta.append(teta[len(teta)-1]+np.pi/10)
-    P = [0.0,np.cos(teta[len(teta)-1]),np.sin(teta[len(teta)-1])]
-
+    #coloca no vetor o angulo da proxima iteracao para calcular o pontencial
+    teta.append(teta[len(teta)-1]+np.pi/20)
+    #atualiza o proximo ponto a ser calculado o potencial
+    P = [0.0,np.sin(teta[len(teta)-1]),np.cos(teta[len(teta)-1])]
+#plota resultados
 fig, ax = plt.subplots()
 matplotlib.rcParams['axes.unicode_minus'] = False
 
 x = np.array(teta[:len(teta)-1],dtype=float)
 y = np.array(res,dtype=float) 
-ax.plot(x,y, 'o')
+ax.plot(x,y, color='red')
 ax.set_title('Using hyphen instead of Unicode minus')
 plt.show()
--- Q3.py
+++ Q3.py
@@ -4,10 +4,6 @@ import matplotlib.pyplot as plt
 import scipy.constants as scc
 from scipy import integrate
 
-#funcao utilizada para realizar a integral do modulo scipy
-def f(x,y):
-    return (1/np.sqrt(1+x**2+y**2))
-
 #funcao que calcula potecial discreto de um elemento de area em relacao a um ponto
 # R = distancia entre o elemento de area e o ponto
 # P = vetor que indica o ponto a ser medido o pontencial
@@ -22,46 +18,40 @@ def potencial_disc(r,P,q):
 # 2*a = lado do quadrado equivalente a area total
 
 def potencial_total(N,P,ro,a):
-    #act = elemento de area atual a ser medido o pontencial no ponto P
     act = [a,a,0]
-    #pot = soma de todos os pontenciais referentes ao elemento de area
     pot = 0
-    #q = carga por elemento de area
     q = ro*(N*N)
     while act[1] > -a:
         act[0] = a
         while act[0] >= -a:
-           pot = pot + potencial_disc(act,P,q)
-           #anda na coordenada x do espaco
-           act[0] -= N
-        #anda na coordenada y do espaco
+            #calcula o potencial de acordo com a distribuicao de carga proposta pelo problema
+            if act[1] >= 0:
+                pot = pot + potencial_disc(act,P,q)
+            elif act[1] < 0:
+                pot = pot + potencial_disc(act,P,-q)
+            act[0] -= N
         act[1] -= N
     return pot
 #inicializacao das variaveis de acordo com o problema
-N = 0.1
-P = [0.0,0.0,1.0]
+teta = [-np.pi/2]
+N = 0.0001
+P = [0.0,np.sin(teta[0]),np.cos(teta[0])]
 a = 0.1
 ro = 10**(-5)
-res = [0.0,0.0,0.0,0.0]
+res = []
+while teta[len(teta)-1] <= np.pi/2:
+    #coloca no vetor o potencial correspondente ao ponto P
+    res.append(potencial_total(N,P,ro,a))
+    #coloca no vetor o angulo da proxima iteracao para calcular o potencial
+    teta.append(teta[len(teta)-1]+np.pi/20)
+    #atualiza o proximo ponto a ser calculado o pontencial
+    P = [0.0,np.sin(teta[len(teta)-1]),np.cos(teta[len(teta)-1])]
+#plota resultados
 fig, ax = plt.subplots()
-#resultado analitico realizado pelo scipy
 matplotlib.rcParams['axes.unicode_minus'] = False
-res_analitc = integrate.nquad(f,[[-0.1,0.1],[-0.1,0.1]]) 
-res[0] = res_analitc[0]
-#resultado para N = 0.1
-res[3] = potencial_total(N,P,ro,a)
 
-#resultado para N = 0.05
-N2 = 0.05
-res[2] = potencial_total(N2,P,ro,a)
-
-#resultado para N = 0.0001
-N3 = 0.0001
-res[1] = potencial_total(N3,P,ro,a)
-
-#plotando pontos
-x = np.array([0.0,N3,N2,N],dtype=float)
+x = np.array(teta[:len(teta)-1],dtype=float)
 y = np.array(res,dtype=float) 
-ax.plot(x,y, 'o')
+ax.plot(x,y, color='red')
 ax.set_title('Using hyphen instead of Unicode minus')
 plt.show()
